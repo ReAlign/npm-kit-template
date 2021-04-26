ed98d47eb226fed42fda15602a33284c1a417141
ReAlign <realign@yeah.net>
2021-04-26 19:11:12 +0800

    re:init

--- sao.js
+++ sao.js
@@ -1,17 +1,6 @@
 module.exports = {
   prompts: {
-    packageName: {
-      message: `package name:`,
-      default: ':folderName:',
-    },
-    description: {
-      message: `Description:`,
-      default: '',
-    },
-    libraryName: {
-      message: `library name:`,
-      default: 'main',
-    }
+    //
   },
   installDependencies: true,
 };
